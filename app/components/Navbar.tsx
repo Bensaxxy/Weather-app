de2f9654598bf7d99c5a5d0d0743da7aabba6f9a
Bensaxxy <benjaminsegun38@gmail.com>
2025-09-12 14:35:08 +0100

    i finished the navbar  interface and i have created the searchbar components and i already added the Heading

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,11 +3,25 @@ import React from "react";
 
 const Navbar = () => {
   return (
-    <div className=" my-5 p-4 md:px-6 lg:px-10 xl:px-20">
+    <div className=" my-5 p-4 md:px-6 lg:px-16 xl:px-28 2xl:px-40">
       {/* logo */}
       <div className=" flex justify-between items-center">
-        <Image src="/images/logo.svg" width={150} height={150} alt="logo" />
-        <div>Units</div>
+        <Image src="/images/logo.svg" width={170} height={170} alt="logo" />
+        <div className=" flex gap-2 items-center cursor-pointer bg-neutral-300/30 py-1 px-3 rounded">
+          <Image
+            src="/images/icon-units.svg"
+            width={14}
+            height={14}
+            alt="unit-icon"
+          />
+          <span className=" font-[500]">Units</span>
+          <Image
+            src="/images/icon-dropdown.svg"
+            width={14}
+            height={14}
+            alt="unit-icon"
+          />
+        </div>
       </div>
     </div>
   );
--- app/components/home.tsx
+++ app/components/home.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Navbar from "./Navbar";
+import SearchBar from "./SearchBar";
 
 const HomePage = () => {
   return (
     <div>
+      {/* navbar */}
       <Navbar />
+      <h1
+        className=" text-5xl text-center my-10"
+        style={{ fontFamily: "var(--font-bricolage-grotesque)" }}
+      >
+        How's the sky looking today?
+      </h1>
+
+      {/* searchbar */}
+      <SearchBar />
     </div>
   );
 };
