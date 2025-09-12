8fb2664108bf7253284f8258f2eaf03feedeb997
Bensaxxy <benjaminsegun38@gmail.com>
2025-09-12 14:07:13 +0100

    still working on the alighnement on my Navbar

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,16 @@
+import Image from "next/image";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className=" my-5 p-4 md:px-6 lg:px-10 xl:px-20">
+      {/* logo */}
+      <div className=" flex justify-between items-center">
+        <Image src="/images/logo.svg" width={150} height={150} alt="logo" />
+        <div>Units</div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
